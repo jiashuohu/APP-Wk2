00df945a52b1499021c48a771996ab836ec806f3
大碩 <loliconwwwwww@gmail.com>
2020-03-14 18:08:41 +0800

    Header

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/Header';
+
+export default function App() {
+  return (
+    <View >
+      <Header />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Header = () => {
+    return (
+        <View style={styles.headerStyle}>
+            <Text style={styles.artistTextStyle}>米津玄師　４thアルバム</Text>
+            <Text style={styles.albumTextStyle}>BOOTLEG</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerStyle: {
+        backgroundColor: "#FFFFFF",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 90,
+        paddingTop: 30,
+        elevation: 2,
+    },
+    artistTextStyle: {
+        fontSize: 15
+    },
+    albumTextStyle: {
+        fontSize: 30
+    },
+});
+
+export default Header;
